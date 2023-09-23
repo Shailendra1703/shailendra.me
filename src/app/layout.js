1c3b18b3e16d15e1859677b4d1400230d7491eb1
Shailendra_Singh <singhshailendra1701@gmail.com>
2023-09-23 14:18:19 +0530

    created footer

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
+import Navbar from "yes@@/ui/Navbar";
 import { Inter } from "next/font/google";
+import Footer from "yes@@/ui/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className="max-w-5xl mx-auto">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,10 @@
 import Image from "next/image";
-import Navbar from "../ui/Navbar";
+
 import Aboutme from "../ui/Aboutme";
 
 export default function Home() {
   return (
     <main className="max-w-5xl mx-auto font-Poppins">
-      <Navbar />
       <Aboutme />
     </main>
   );
